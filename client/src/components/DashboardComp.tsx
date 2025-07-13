c8ebf0acee3b256728dd147de9e110a97652d572
Karthik R <karthikeya.rachamolla@gmail.com>
2025-07-12 20:44:19 -0400

    basic grid layout design for dashboard

--- client/src/components/DashboardComp.tsx
+++ client/src/components/DashboardComp.tsx
@@ -1,8 +1,26 @@
 export default function DashboardComp(){
     return (
-        <div className="">
-            Hi
-
+        <div className="grid grid-cols-[20%_60%_20%] border grid-rows-[5%_1fr] h-screen">
+            <div className="row-start-1 col-start-1 w-full border">
+                Profile
+            </div>
+            <div className="row-start-1 col-start-2 h-full border">
+                Dashboard title
+            </div>
+            <div className="row-start-1 col-start-3 h-full border">
+                Notifications
+            </div>
+            <div className="row-start-2 col-start-1 flex justify-center items-center h-full border">
+                Profile content
+            </div>
+            <div className="row-start-2 col-start-2 h-full flex justify-center items-center border">
+                Main Content
+            </div>
+            <div className="row-start-2 col-start-3 h-full flex items-center justify-center border">
+                Notifications Content
+            </div>
+            
+            
         </div>
     )
 }
--- client/src/components/DashboardLayout.tsx
+++ client/src/components/DashboardLayout.tsx
@@ -4,7 +4,7 @@ export default function DashboardLayout() {
   return (
     <div className="bg-white min-h-screen">
       <div className="flex flex-col items-center min-h-screen">
-        <div className="w-screen font-sans-serif h-screen max-h-screen mx-auto">
+        <div className="w-screen font-sans-serif mx-auto">
           <Outlet />
         </div>
       </div>
